786b505464c3c9e853e4ec123724299bfe0994c3
Anshuman <makemefriend.anshu@gmail.com>
2024-09-21 12:14:09 +0530

    Show book list
    Show book list
    
    Update home.html.heex

--- lib/anuplalmandal_web.ex
+++ lib/anuplalmandal_web.ex
@@ -17,7 +17,7 @@ defmodule AnuplalmandalWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt pdfs)
 
   def router do
     quote do
